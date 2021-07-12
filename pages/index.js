47fbc4c27736d10b695649274893fee71d135405
garrick <yegarrick12@gmail.com>
2021-07-11 19:14:15 -0600

    editable

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,23 @@ import ModalFooter from "@material-tailwind/react/ModalFooter";
 import { useState } from "react";
 import { db } from "../firebase";
 import firebase from "firebase";
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import DocumentRow from "../components/DocumentRow";
 
 export default function Home() {
   const [session] = useSession();
+  if (!session) return <Login />;
+
   const [showModal, setShowModal] = useState(false);
   const [input, setInput] = useState("");
-
-  if (!session) return <Login />;
+  const [snapshot] = useCollectionOnce(
+    db
+      .collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .orderBy("timestamp", "desc")
+  );
+  // console.log(snapshot?.docs[0].id);
 
   const createDocument = () => {
     if (!input) return;
@@ -103,10 +113,19 @@ export default function Home() {
       <section className="bg-white px-10 md:px-0">
         <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
           <div className="flex items-center justify-between pb-5">
-            <h2 className="font-medium flex-glow">My Documents</h2>
+            <h2 className="font-medium flex-grow">My Documents</h2>
             <p className="mr-12">Date Created</p>
             <Icon name="folder" size="3xl" color="gray" />
           </div>
+
+          {snapshot?.docs.map((doc) => (
+            <DocumentRow
+              key={doc.id}
+              id={doc.id}
+              fileName={doc.data().fileName}
+              date={doc.data().timestamp}
+            />
+          ))}
         </div>
       </section>
     </div>
